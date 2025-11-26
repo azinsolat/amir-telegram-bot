96512d9d874827e0eef1153b6030fe53c2415233
azinsolat <azinsolat@gmail.com>
2025-11-27 01:51:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -238,10 +238,11 @@ if __name__ == "__main__":
      app.add_handler(CommandHandler("amir", amir_command))
 
 
-     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , handle_massage))
+     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , handle_message))
      app.add_error_handler(error)
 
      print("polling")
      app.run_polling()
+
 
 
